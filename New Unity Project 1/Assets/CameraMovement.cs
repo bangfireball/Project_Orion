4d76c40ad36ef33f6a35246f10222f5b739cf875
unknown <riley_chadwick1@HP2UA5321B1D.student.eku.edu>
2016-01-30 11:27:20 -0500

    Modifyed the VRTest scene

---
+++ New Unity Project 1/Assets/CameraMovement.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMovement : MonoBehaviour {
+
+    bool isMoving = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Cardboard.SDK.Triggered == true)
+        {
+            isMoving = !isMoving;
+        }
+
+        if (isMoving)
+        {
+
+        }
+	}
+
+    private void SDK_OnTrigger()
+    {
+        throw new System.NotImplementedException();
+    }
+}
